441d20aac964029be9e8d2aea9dbc410c5e7e38f
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-03-20 11:48:34 +0100

    home page w styling to work for react. updated classnames to be specific for page as default for css in react is to be global.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
+import './Home.css'
+import hero from '../assets/hero.jpg'
 
 export default function Home() {
     return (
-        <h1>HomePage</h1>
+        <>
+            <section className="hero-content-container">
+                <div className="hero-content">
+                    <h1>Nadine Lindell</h1>
+                    <h2>Fullstack Developer</h2>
+                    <p>.NET Fullstack developer student with a passion for problem solving & a sharp eye for details.
+                        I'm
+                        exited to learn more and become at least 1% better each day. Former store-manager with a big
+                        creative streak.</p>
+                    <a href="portfolio.html">Portfolio</a>
+                    <a href="contact.html">Contact</a>
+                </div>
+                <div className="heroimg-container">
+                    <img className="hero-img" src={hero}
+                        alt="neonSign: glad You are here" />
+                </div>
+            </section>
+        </>
     )
 }
