e3e15a9be145ef31e63ceae91cece446c690aa53
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-03-20 08:51:50 +0100

    browser router w import react-router-dom. added all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/home'
+import About from './pages/about.jsx'
+import Contact from './pages/contact'
+import Cv from './pages/cv'
+import Portfolio from './pages/portfolio'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cv" element={<Cv />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Cv.jsx
@@ -0,0 +1,6 @@
+
+export default function Cv() {
+    return (
+        <h1>Cv</h1>
+    )
+}
